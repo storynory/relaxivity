b25e0eebeb9db4019442d81aee1ef54ec301505e
hugh Fraser <hugh@storynory.com>
2021-09-18 07:33:32 +0100

    localhost odd stuff

--- .eleventy.js
+++ .eleventy.js
@@ -5,17 +5,18 @@ const webp  = require("./_includes/filters/webp");
 
 
 module.exports = function(eleventyConfig) {
-// eleventyConfig.addPlugin(pluginRss);
- 
+
+  // eleventyConfig.addPlugin(pluginRss);
+
   let markdownIt = require("markdown-it");
   let options = {
     html: true,
     breaks: true,
     linkify: true
   };
+  
 
 eleventyConfig.addNunjucksFilter("webp", webp);
-
 
 
    // Get the first `n` elements of a collection.
@@ -27,4 +28,7 @@ eleventyConfig.addFilter("head", (array, n) => {
 
     return array.slice(0, n);
   });
+
+
+  
 };
