2b2fe8ea6a09654e9877a6e94b8112bddffa8a15
hugh Fraser <hugh@storynory.com>
2021-09-19 12:39:08 +0100

    service worker update

--- _site/serviceworker.js
+++ _site/serviceworker.js
@@ -1,107 +1,54 @@
-'use strict';
+var CACHE_VERSION = 3;
+var CURRENT_CACHES = {
+    prefetch: 'static' + CACHE_VERSION
+};
 
-// Licensed under a CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
-// http://creativecommons.org/publicdomain/zero/1.0/
+self.addEventListener('install', function (event) {
+    var urlsToPrefetch = [
+        './static/pre_fetched.txt',
+        './static/pre_fetched.html'
+    ];
 
-(function () {
-
-    // Update 'version' if you need to refresh the cache
-    var staticCacheName = 'static';
-    var version = 'v1::';
-
-    // Store core files in a cache (including a page to display when offline)
-    function updateStaticCache() {
-        return caches.open(version + staticCacheName)
-            .then(function (cache) {
-                return cache.addAll([
-                    "/index.html",
-                    "https://cdn.jsdelivr.net/npm/fuse.js/dist/fuse.js",
-                    "/styles/app.css"
-
-                ]);
+    event.waitUntil(
+        caches.open(CURRENT_CACHES.prefetch).then(function (cache) {
+            return cache.addAll(urlsToPrefetch).then(function () {
+                console.log('All resources have been fetched and cached.');
+                // skipWaiting() allows this service worker to become active
+                // immediately, bypassing the waiting state, even if there's a previous
+                // version of the service worker already installed.
+                self.skipWaiting();
             });
-    };
+        }).catch(function (error) {
+            // This catch() will handle any exceptions from the caches.open()/cache.addAll() steps.
+            console.error('Pre-fetching failed:', error);
+        })
+    );
+});
 
-    self.addEventListener('install', function (event) {
-        event.waitUntil(updateStaticCache());
+self.addEventListener('activate', function (event) {
+    // clients.claim() tells the active service worker to take immediate
+    // control of all of the clients under its scope.
+    self.clients.claim();
+
+    // Delete all caches that aren't named in CURRENT_CACHES.
+    // While there is only one cache in this example, the same logic will handle the case where
+    // there are multiple versioned caches.
+    var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function (key) {
+        return CURRENT_CACHES[key];
     });
 
-    self.addEventListener('activate', function (event) {
-        event.waitUntil(
-            caches.keys()
-                .then(function (keys) {
-                    // Remove caches whose name is no longer valid
-                    return Promise.all(keys
-                        .filter(function (key) {
-                            return key.indexOf(version) !== 0;
-                        })
-                        .map(function (key) {
-                            return caches.delete(key);
-                        })
-                    );
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (expectedCacheNames.indexOf(cacheName) === -1) {
+                        // If this cache name isn't present in the array of "expected" cache names,
+                        // then delete it.
+                        console.log('Deleting out of date cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
                 })
-        );
-    });
-
-    self.addEventListener('fetch', function (event) {
-        var request = event.request;
-        // Always fetch non-GET requests from the network
-        if (request.method !== 'GET') {
-            event.respondWith(
-                fetch(request)
-                    .catch(function () {
-                        return caches.match('/offline.html');
-                    })
             );
-            return;
-        }
-
-        // For HTML requests, try the network first, fall back to the cache, finally the offline page
-        if (request.headers.get('Accept').indexOf('text/html') !== -1) {
-            // Fix for Chrome bug: https://code.google.com/p/chromium/issues/detail?id=573937
-            if (request.mode != 'navigate') {
-                request = new Request(request.url, {
-                    method: 'GET',
-                    headers: request.headers,
-                    mode: request.mode,
-                    credentials: request.credentials,
-                    redirect: request.redirect
-                });
-            }
-            event.respondWith(
-                fetch(request)
-                    .then(function (response) {
-                        // Stash a copy of this page in the cache
-                        var copy = response.clone();
-                        caches.open(version + staticCacheName)
-                            .then(function (cache) {
-                                cache.put(request, copy);
-                            });
-                        return response;
-                    })
-                    .catch(function () {
-                        return caches.match(request)
-                            .then(function (response) {
-                                return response || caches.match('/offline.html');
-                            })
-                    })
-            );
-            return;
-        }
-
-        // For non-HTML requests, look in the cache first, fall back to the network
-        event.respondWith(
-            caches.match(request)
-                .then(function (response) {
-                    return response || fetch(request)
-                        .catch(function () {
-                            // If the request is for an image, show an offline placeholder
-                            if (request.headers.get('Accept').indexOf('image') !== -1) {
-                                return new Response('<svg width="400" height="300" role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', { headers: { 'Content-Type': 'image/svg+xml' } });
-                            }
-                        });
-                })
-        );
-    });
-
-})();
+        })
+    );
+});
