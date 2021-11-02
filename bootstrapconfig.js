a5a16dddf37820a2b5e457c133fe24681a66b86d
hugh Fraser <hugh@storynory.com>
2021-11-02 20:15:11 +0000

    green willow

--- bootstrapconfig.js
+++ bootstrapconfig.js
@@ -6,10 +6,10 @@ prefixer = require('autoprefixer')
 const purge = require('@fullhuman/postcss-purgecss')
 //sass.render({file: scss_filename}, function(err, result) { /* ... */ });
 const plugins = [
-   prefixer,
-   purge({
-      content: ['./_site/*.html', '**/*.html',]
-   })
+    prefixer,
+    purge({
+        content: ['./_site/*.html', '**/*.html',]
+    })
 
 ]
 
@@ -17,14 +17,13 @@ const plugins = [
 
 
 sass.render({ file: "./css/styles.scss", outputStyle: "compressed", fiber: Fiber }, function (err, result) {
-   //fs.writeFile('./_site/styles/app.css', result.css, () => true)
-   let code = result.css.toString();
+    //fs.writeFile('./_site/styles/app.css', result.css, () => true)
+    let code = result.css.toString();
 
 
-   postcss(plugins).process(code, { from: "./css/styles.scss", to: './_includes/styles/app3.css' }).then(result => {
+    postcss(plugins).process(code, { from: "./css/styles.scss", to: './_includes/styles/app3.css' }).then(result => {
 
-      fs.writeFile('./_includes/styles/app2.css', result.css, () => true)
-   })
+        fs.writeFile('./_includes/styles/app2.css', result.css, () => true)
+    })
 
 });
-
---
+++ eleventy.js
@@ -0,0 +1,45 @@
+const webp = require("./_includes/filters/webp");
+const datenow = require("./_includes/filters/getlatest");
+//const pluginRss = require("@11ty/eleventy-plugin-rss");
+//const htmlmin = require('html-minifier')
+
+
+module.exports = function (eleventyConfig) {
+
+    // eleventyConfig.addPlugin(pluginRss);
+
+
+
+
+    eleventyConfig.addNunjucksFilter("webp", webp);
+    eleventyConfig.addNunjucksFilter("datenow", datenow);
+
+
+    // Get the first `n` elements of a collection.
+
+    eleventyConfig.addFilter("head", (array, n) => {
+        if (n < 0) {
+            return array.slice(n);
+        }
+
+        return array.slice(0, n);
+    });
+
+    /** 
+      eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    
+        if (outputPath.endsWith('.html')) {
+    
+          let minified = htmlmin.minify(content, {
+            useShortDoctype: true,
+            removeComments: true,
+            collapseWhitespace: true
+          });
+          return minified;
+        }
+    
+        return content;
+      });
+     **/
+
+};
