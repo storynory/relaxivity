f77c8b17a363a0bc69c596b64bf94b2bcd9945f9
hugh Fraser <hugh@storynory.com>
2023-05-04 20:57:04 +0100

    server

--- tina/config.js
+++ tina/config.js
@@ -171,9 +171,7 @@ const schema = defineSchema({
 export const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
   branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH || // custom branch env override
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
-    process.env.HEAD, // Netlify branch env
+    process.env.NEXT_PUBLIC_TINA_BRANCH,
   token: process.env.TINA_TOKEN,
   media: {
     // If you wanted cloudinary do this
